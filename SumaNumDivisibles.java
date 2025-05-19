1e0fb55afc8af2de08a79174728a2118c6bda2a1
BlasQA <blasquinteros98@gmail.com>
2025-05-18 21:02:07 -0300

    Update SumaNumDivisibles.java
    CorrectionDivisible

--- SumaNumDivisibles.java
+++ SumaNumDivisibles.java
@@ -1,18 +1,18 @@
-import Java.util.Scanner;
+import java.util.Scanner;
 
-static class "SumaNumDivisibles" {
-    public static void (String[] ars){
+public class SumaNumDivisibles {
+    public static void main(String[] ars){
         Scanner readln = new Scanner (System.in);
-        System.println.out("Ingrese el primer numero");
+        System.out.println ("Ingrese el primer numero");
         int num1 = readln.nextInt();
-        System.println.out("Ingrese el segundo numero");
+        System.out.println("Ingrese el segundo numero");
         int num2 = readln.nextInt();
         int num3 = num1 + num2;
-        if (num1%num2 = 0) {
-            System.out.println ("La suma de los numeros es " + num3 + " y " + num1 + " es divisible por " + num 2)
+        if (num1 % num2 == 0) {
+            System.out.println ("La suma de los numeros es " + num3 + " y " + num1 + " es divisible por " + num2);
         }
         else {
-            System.out.println ("La suma de los numeros es " + num3 + " y " + num1 + " NO es divisible por " + num 2)
+            System.out.println ("La suma de los numeros es " + num3 + " y " + num1 + " NO es divisible por " + num2);
         } 
-        }
+    }
 }
